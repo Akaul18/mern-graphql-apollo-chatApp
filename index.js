945f4638b2b6f4a8ad6378cf097b9a2066e949e2
kaul <ankurkaul1894@gmail.com>
2019-12-04 00:20:14 -0800

    converted backend from express-graphql server to apollo server

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const express = require("express");
-const graphqlHTTP = require("express-graphql");
-const { buildSchema } = require("graphql");
+const { ApolloServer, gql } = require("apollo-server");
 const crypto = require("crypto");
-const app = express();
 
 const db = {
   users: [
@@ -26,88 +23,55 @@ const db = {
   ]
 };
 
-class User {
-  constructor(user) {
-    Object.assign(this, user);
+const typeDefs = gql`
+  type Query {
+    users: [User!]!
+    user(id: ID!): User
+    messages: [Message!]!
   }
 
-  get messages() {
-    return db.messages.filter(message => message.userId === this.id);
+  type Mutation {
+    addUser(email: String!, name: String): User
   }
-}
 
-const schema = buildSchema(`
-    type Query{
-        users: [User!]!
-        user(id: ID!): User
-        messages: [Message!]!
+  type User {
+    id: ID!
+    email: String!
+    name: String
+    avatarUrl: String
+    messages: [Message!]!
+  }
+
+  type Message {
+    id: ID!
+    body: String!
+    create_time: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    users: () => db.users,
+    user: (root, args) => db.users.find(user => user.id === args.id),
+    messages: () => db.messages
+  },
+
+  Mutation: {
+    addUser: (root, { email, name }) => {
+      const user = {
+        id: crypto.randomBytes(10).toString("hex"),
+        email,
+        name
+      };
+      db.users.push(user);
+      return user;
     }
+  },
 
-    type Mutation{
-        addUser(email:String!,name:String): User
-    }
-
-    type User{
-        id: ID!,
-        email: String!,
-        name: String,
-        avatarUrl: String
-        messages: [Message!]!
-    }
-
-    type Message{
-        id: ID!
-        body: String!
-        create_time: String
-    }
-`);
-
-const rootValue = {
-  //   message: () => "graphql works" //  it is the resolver and the name should be same as type key
-  users: () => db.users.map(user => new User(user)),
-  user: args => db.users.find(user => user.id === args.id),
-  messages: () => db.messages,
-  addUser: ({ email, name }) => {
-    // args is the arguments that will be passed with your query to the graphql endpoint we can destructure args with {email, name}
-    const user = {
-      id: crypto.randomBytes(10).toString("hex"),
-      email,
-      name
-    };
-    db.users.push(user);
-    return user;
+  User: {
+    messages: user => db.messages.filter(message => message.userId === user.id) //here the argument root is actually user
   }
 };
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema,
-    rootValue,
-    graphiql: true
-  })
-); //we use this to hook up express with graphql
-
-app.listen(3000, () => console.log(`listening on port 3000`));
-
-// graphql(
-//   schema,
-
-//   `
-//     {
-//       users {
-//         id
-//         email
-//       }
-//     }
-//   `,
-
-//   //   `
-//   //     {
-//   //       message
-//   //     }
-//   //   `, //  query(this will be the actual  graphql query that we need to execute)
-//   rootValue //its a resolver or root resolver function
-// )
-//   .then(res => console.dir(res, { depth: null }))
-//   .catch(console.error); //graphql function will give us a promise so we can use then
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => console.log(url));
